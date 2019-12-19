21ae8bca70960df7fa965fc467232413d875f524
matheus-fls <matheus_fls@yahoo.com.br>
2019-12-19 20:56:14 -0300

    Fix errors

--- src/App.js
+++ src/App.js
@@ -8,8 +8,8 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit 
-          <code>src/App.js</code> 
+          Edit
+          <code>src/App.js</code>
           and save to reload.
         </p>
         <a
