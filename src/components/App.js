764d1cce1e6a0987b532f17811ac2141a4314937
matheus-fls <matheus_fls@yahoo.com.br>
2020-01-14 17:43:22 -0300

    Add state and handleClick methods to App

--- src/components/App.js
+++ src/components/App.js
@@ -1,20 +1,30 @@
 import React from 'react';
 import Display from './Display';
 import ButtonPanel from './ButtonPanel';
-import calculate from '../logic/calculate'; {/* eslint-disable-line */}
+import calculate from '../logic/calculate';
 import '../App.css';
 
 class App extends React.Component {
   constructor(props) {
     super(props);
-    this.state = { };
+    this.state = {
+      total: null,
+      next: null,
+      operation: null,
+    };
+  }
+
+  handleClick(buttonName) {
+    this.setState({
+      total: calculate(this.state, buttonName),
+    })
   }
 
   render() {
     return (
       <div className="app">
-        <Display />
-        <ButtonPanel />
+        <Display result={this.state.total || this.state.next} />
+        <ButtonPanel clickHandler={this.handleClick()} />
       </div>
     );
   }
